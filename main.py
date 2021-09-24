b810b9588818d6f455b90fb5cf6f72aed85fdd04
Default <eugenejmiller99@gmail.com>
2021-09-23 21:27:01 -0600

    function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from wos import WosClient
 import wos.utils
 
-def print_hi(name):
+def print_author(author):
     with WosClient('eugene.miller@colorado.edu', 'F+KB;!zkfKN7F:') as client:
-        print(wos.utils.query(client, 'AU=Knuth Donald'))
+        print(wos.utils.query(client, ('AU=' + author)))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_author('Knuth Donald')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
